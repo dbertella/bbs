d2abd00d9cbde7851bb47830301dfcc283e92827
Daniele Bertella <daniele.bertella@gmail.com>
2025-06-08 13:26:42 +0100

    Update color codes and title

--- src/utils/house.ts
+++ src/utils/house.ts
@@ -8,3 +8,13 @@ export const formatHouseName = (house: string): string => {
             return house;
     }
 }; 
+export const houseBgColor = (house: string): string => {
+    switch (house) {
+        case 'SERRA':
+            return 'bg-blue-500';
+        case 'SOLARO':
+            return 'bg-emerald-500';
+        default:
+            return house;
+    }
+}; 
